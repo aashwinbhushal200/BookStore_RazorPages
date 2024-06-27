1562c12bd03554944138413fd2dd4ab5a5754772
aashwinbhushal961 <aashwinbhushal961@outlook.com>
2024-06-27 07:21:33 +0530

    Added-dbContext+migration

--- BookStore_RazorPages/Program.cs
+++ BookStore_RazorPages/Program.cs
@@ -1,7 +1,13 @@
+using BookStore_RazorPages.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+//AddDbContext
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
